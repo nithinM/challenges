d247bfeb4189e5e7a7be14c895f75883db7ea17f
Nithin Madhuranga <nithin@axis.lk>
2018-09-23 19:31:13 +0530

    - Update Webpack npm module latest v4 - Configure Webpack to handle production build env and development env.

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const isTest = String(process.env.NODE_ENV) === "test";
+
+module.exports = {
+  presets: [
+    ["env", { modules: isTest ? "commonjs" : false }],
+    "react"
+  ],
+  plugins: [
+    "transform-class-properties",
+    "transform-object-rest-spread",
+    "syntax-dynamic-import"
+  ]
+};
