69b6a9bfe874efb265d140dfd39d5d8c5a8cacb3
Nithin Madhuranga <nithin@axis.lk>
2018-10-03 00:26:52 +0530

    Setup dummy data for unit tests

---
+++ src/__test__/fixtures/charities.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    name: "Baan Kru Noi",
+    image: "baan-kru-noi.jpg",
+    currency: "THB"
+  },
+  {
+    id: 2,
+    name: "Habitat for Humanity Thailand",
+    image: "habitat-for-humanity-thailand.jpg",
+    currency: "THB"
+  },
+  {
+    id: 3,
+    name: "Paper Ranger",
+    image: "paper-ranger.jpg",
+    currency: "THB"
+  }
+];
