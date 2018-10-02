2c06cb37f4461a6626f4ee139103dc259d1ffc69
Nithin Madhuranga <nithin@axis.lk>
2018-10-03 00:30:57 +0530

    Setup Unit Test for Utils

---
+++ src/__test__/util/getTotalDonations.test.js
@@ -0,0 +1,14 @@
+import getTotalDonations from "../../util/getTotalDonations";
+import payments from "../fixtures/payments";
+
+describe("getTotalDonations", () => {
+  test("should calculate total donations for no payments correctly", () => {
+    const donations = [];
+    expect(getTotalDonations(donations)).toBe(0);
+  });
+
+  test("should calculate total donations for payments correctly", () => {
+    const donations = payments;
+    expect(getTotalDonations(donations)).toBe("80 THB");
+  });
+});
